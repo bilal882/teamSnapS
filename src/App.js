dcadb82a8640be75d5cd2d769d85adcbea27f169
Muhammad Bilal <bilaliqbal78692110@gmail.com>
2022-09-17 18:03:06 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,88 +1,20 @@
 import React from 'react'
-import "./App.css"
 import "bootstrap/scss/bootstrap.scss";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css"
+import Header from './components/Header/Header';
+import Table from './components/Table/Table';
+import Pricing from './components/Pricing/Pricing';
 
 
 export default function App() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-bg navbar-dark sticky-top top-0">
-        <div class="container text-center">
-          <a class="navbar-brand" href="#">Team Snap</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item dropdown">
-                <a class="nav-link active" >
-                  Products
-                  <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="dropdown-menu display">
-                  <div className="dropdown-items text-start" style={{ minWidth: "250px" }}>
-                    <a className='nav-link ms-3' href="#">Clubs & Leagues</a>
-                    <li><a href="#" className='nav-links'>For clubs & associations with multiple teams</a></li>
-                    <li><a href="#" className='nav-links'>Sports</a></li>
-                    <li><a href="#" className='nav-links'>Features</a></li>
-                    <li><a href="#" className='nav-links'>Get a demo</a></li>
-                  </div>
-                  <div className="dropdown-items text-start" style={{ minWidth: "250px" }}>
-                    <a className='nav-link ms-3' href="#">Teams</a>
-                    <li><a href="#" className='nav-links'>The easiest way to manage a team</a></li>
-                    <li><a href="#" className='nav-links'>Sports</a></li>
-                  </div>
-                  <div className="dropdown-items text-start" style={{ minWidth: "250px" }}>
-                    <a className='nav-link ms-3' href="#">Tournaments</a>
-                    <li><a href="#" className='nav-links'>Build and manage your events in a snap</a></li>
-                    <li><a href="#" className='nav-links'>Scheduling</a></li>
-                    <li><a href="#" className='nav-links'>Registration</a></li>
-                  </div>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Pricing</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link active" >
-                  Company
-                  <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="dropdown-menu">
-                  <div className="dropdown-items w-100 text-start" style={{ minWidth: "250px" }}>
-                    <a className='nav-links' href="#">About</a>
-                    <a className='nav-links' href="#">For Brands</a>
-                    <a className='nav-links' href="#">Career</a>
-                  </div>
-                </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link active">
-                  Resourses
-                  <i class="fa-solid fa-angle-down"></i>
-                </a>
-                <ul class="dropdown-menu">
-                  <div className="dropdown-items w-100 text-start" style={{ minWidth: "250px" }}>
-                    <a className='nav-links' href="#">Blog</a>
-                    <a className='nav-links' href="#">Community Impact</a>
-                    <a className='nav-links' href="#">Off-Season</a>
-                    <a className='nav-links' href="#">Tips for Student Athletes</a>
-                    <a className='nav-links' href="#">Coaches Corner</a>
-                    <a className='nav-links' href="#">Return to play</a>
-                    <a className='nav-links' href="#">Clubs & leagues Resources</a>
-                    <a className='nav-links' href="#">Help center</a>
-                    <a className='nav-links' href="#">Api Docs</a>
-                  </div>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Header />
+      <Pricing />
+      <div className="my-5"></div>
+      {/* <Table /> */}
     </>
   )
 }
---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Pricing.css"
+
+export default function Pricing() {
+    return (
+        <>
+            <div className="top-part text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-8">
+                            <h1>Go Premium</h1>
+                            <h5>Our Premium plan is the best option for most teams — it combines all of TeamSnap's most useful features in one package for a great price.</h5>
+                        </div>
+                        <div className="col-4">
+                            <img src="https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/new/roster-39237d0433df600285115fb1788a64be.png" className='d-none d-md-flex img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center">
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="tab-btn active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="tab-btn" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+                            </li>
+                        </ul>
+                        <div class="tab-content mt-5" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
+                            <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
